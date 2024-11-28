368ed33aed47506e013e4df94014b1c06caf2db8
anusornW <78833104+anusornW@users.noreply.github.com>
2024-11-28 14:15:37 +0700

    commit 1 great done

---
+++ lab1minx.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+	int num1=15;
+	int num2=10;
+	if (num1>num2) {
+		printf("num1 greater than num2");
+	}
+	if (num2>num1) {
+		printf("num2 greater than num1");
+	}
+}
